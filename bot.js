42524004a63c4d36c155977e06b350d8b0865214
youssefeno <45043899+youssefeno@users.noreply.github.com>
2019-07-10 15:20:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -20,7 +20,7 @@ const gif = require("gif-search");
 
 const client = new Discord.Client({disableEveryone: true});
 
-const prefix = "2";
+const prefix = "R";
 /////////////////////////
 ////////////////////////
 
@@ -349,7 +349,7 @@ client.on('ready', () => {
       console.log(`ON ${client.guilds.size} Servers '     Script By : EX Clan ' `);
     console.log(`----------------`);
   console.log(`Logged in as ${client.user.tag}!`);
-client.user.setGame(`2play | Last Music`,"http://twitch.tv/Death Shop")
+client.user.setGame(`Rplay | Singel ; .22`,"http://twitch.tv/Death Shop")
 client.user.setStatus("dnd")
 });
 
